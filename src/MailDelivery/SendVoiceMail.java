ea42e50cc5e8e92726f40a24dcb1a9e2cd1d3fba
Abhijeet Banerjee <abhijeet.feb@gmail.com>
2016-08-03 10:42:32 +0530

    [CUMI]

--- src/MailDelivery/SendVoiceMail.java
+++ src/MailDelivery/SendVoiceMail.java
@@ -1,8 +1,13 @@
 package MailDelivery;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Base64;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,7 +20,7 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class SendVoiceMail {
-	private static String requestUrl = "https://ucbu-aricent-vm88.cisco.com:8443/vmrest/messages";
+	private static String requestUrl = "https://bgl-cxn-st-3.cisco.com:8443/vmrest/messages?userobjectid=9d7f24aa-8e59-4d67-9e43-7dd55d90040e";
 	File file1 = null;
 	File file2 = null;
 	File voiceMessage = null;
@@ -27,25 +32,53 @@ public class SendVoiceMail {
 
 	}
 
-	void sendMail() {
+	void sendMail() throws IOException {
 		RestTemplate restTemplate = new RestTemplate(getClientHttpRequestFactory(true));
 		restTemplate.setErrorHandler(new ErrorResponseHandler());
 
-		file1 = new File("C:/Users/abhibane/Desktop/cumi/message.xml");
-		System.out.println(file1.length());
-		file2 = new File("C:/Users/abhibane/Desktop/cumi/recipient.xml");
-		System.out.println(file2.length());
-		voiceMessage = new File("C:/Users/abhibane/Desktop/cumi/VoiceMessage.wav");
-		System.out.println(voiceMessage.exists());
-
-		String userPassword = "admin:ecsbulab";
+		String userPassword = "Abhijeet:Ecsbulab1";
 		String basicAuth = Base64.getEncoder().encodeToString(userPassword.getBytes());
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-		headers.add(HttpHeaders.AUTHORIZATION, basicAuth);
+		headers.add(HttpHeaders.AUTHORIZATION, "Basic " +basicAuth);
+		
+		String subject = "Voicemail from Prashant Prashant (ext - 1037)"; 
+		String message = String.format("{\"Subject\" : \"%s\", \"Sensitivity\" : \"Normal\", \"Priority\" : \"Normal\" }", subject);
+
+		// creating an HttpEntity for the message part
+		HttpHeaders messageHeader = new HttpHeaders();
+		messageHeader.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> messageHttpEntity = new HttpEntity<>(message, messageHeader);
+
+		String calleeObjectId = "8ccfcf71-0c07-4dfc-b1c4-00933ba72d13";
+		String recipient = String.format("{\"Recipient\" : { \"Type\": \"TO\", \"Address\" : { \"Type\": \"SUBSCRIBER\", \"ObjectId\" : \"%s\"} } }", 
+		calleeObjectId);
+
+		// creating an HttpEntity for the recipient part
+		HttpHeaders recipientHeader = new HttpHeaders();
+		recipientHeader.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> recipientHttpEntity = new HttpEntity<>(recipient, recipientHeader);
+
+		// creating an HttpEntity for the binary part
+		HttpHeaders audioHeader = new HttpHeaders();
+		audioHeader.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		InputStream audioStream = new FileInputStream("C:/Users/abhibane/Desktop/cumi/AvAfterMsgMenu001.wav");
+		ByteArrayResource byteArrayResource = new ByteArrayResource(IOUtils.toByteArray(audioStream));
+		HttpEntity<ByteArrayResource> audioHttpEntity = new HttpEntity<>(byteArrayResource, audioHeader);
+		//audioStream.close();
+
 
 		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("message", messageHttpEntity);
+		map.add("recipient", recipientHttpEntity);
+		map.add("VoiceMessage.wav", audioHttpEntity);
+
+		HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<LinkedMultiValueMap<String, Object>>(
+		map, headers);
+		System.out.println(requestEntity.getBody());
+		System.out.println(requestEntity.getHeaders());
+		/*LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
 		map.add("filename", voiceMessage);
 		map.add("ArrivalTime", 0);
 		map.add("fromSub", false);
@@ -56,23 +89,9 @@ public class SendVoiceMail {
 		HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<LinkedMultiValueMap<String, Object>>(
 				map, headers);
 		System.out.println(requestEntity);
-		entity = restTemplate.exchange(requestUrl, HttpMethod.POST, requestEntity, String.class);
-
-		// MultipartPostMethod mPost = new MultipartPostMethod(url);
-
-		// mPost.setRequestHeader ("Authorization", "Basic " + encoding);
-		// mPost.setRequestHeader("Content-Type","multipart/form-data");
-		// mPost.addPart(new FilePart("ctr", file1, "text/xml","ISO-8859-1"));
-		// mPost.addPart(new FilePart("ctr", file2, "text/xml","ISO-8859-1"));
-		// mPost.addPart(new FilePart("ctr1", file3, "audio/wav","ISO-8859-1"));
-		// mPost.addPart(new FilePart("ctr2", file4, "audio/wav","ISO-8859-1"));
-		// mPost.addPart(new FilePart("ctr2", file5, "audio/wav","ISO-8859-1"));
-		// client.executeMethod( mPost );
-		// String response1 = mPost.getResponseBodyAsString();
-		// System.out.println( response1 );
-		// System.out.println("------------------------------------------");
-		// }
-	}
+*/		entity = restTemplate.exchange(requestUrl, HttpMethod.POST, requestEntity, String.class);
+		System.out.println(entity.getStatusCode());
+			}
 
 	/**
 	 * method to create http client factory
